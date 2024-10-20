32cb9f64588b41007ca725eca000d1ad617e7c50
mehrsam88 <mehrsammohseni@gmail.com>
2024-10-20 18:23:47 +0330

    first upload

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+var inputresult = [];
+function inputs_data() {
+  const data_input = document.querySelectorAll("input, textarea");
+  const Error = document.querySelector("#error");
+  const obj = {};
+  let check = true;
+  for (let i = 0; i < data_input.length; i++) {
+    if (data_input[i].value == "") {
+      data_input[i].style.border = "2px solid #e74c3c";
+      Error.style.display = "flex";
+      check = false;
+    } else {
+      data_input[i].style.border = "";
+      obj[data_input[i].id] = data_input[i].value;
+    }
+  }
+  if (check === true) {
+    Error.style.display = "none";
+    inputresult.push(obj);
+    console.log(inputresult);
+    for (let i = 0; i < data_input.length; i++) {
+      data_input[i].value = "";
+    }
+  }
+}
